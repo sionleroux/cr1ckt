fc0f168fd7be852294b404d9a49ee931be7d404d
Siôn le Roux <sinisterstuf@gmail.com>
2021-11-03 00:13:54 +0100

    Load cricket image and draw to screen

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
+// Copyright 2021 Siôn le Roux.  All rights reserved.
+
 package main
 
 import (
+	"embed"
+	"image"
+	"image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
+
+//go:embed assets/*.png
+var assets embed.FS
 
 func main() {
 	gameWidth, gameHeight := 640, 480
@@ -13,12 +21,13 @@ func main() {
 	ebiten.SetWindowTitle("cr1ck_t")
 	ebiten.SetCursorMode(ebiten.CursorModeHidden)
 
-	game := &Game{
-		Width:  gameWidth,
-		Height: gameHeight,
-	}
+	cricket := NewObjectFromImage(loadImage("assets/cricket.png"))
 
-	// go NewGame(game)
+	game := &Game{
+		Width:   gameWidth,
+		Height:  gameHeight,
+		Cricket: cricket,
+	}
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
@@ -27,8 +36,9 @@ func main() {
 
 // Game represents the main game state
 type Game struct {
-	Width  int
-	Height int
+	Width   int
+	Height  int
+	Cricket *Object
 }
 
 // Update calculates game logic
@@ -38,9 +48,45 @@ func (g *Game) Update() error {
 
 // Draw handles rendering the sprites
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.DrawImage(g.Cricket.Image, g.Cricket.Op)
 }
 
 // Layout is hardcoded for now, may be made dynamic in future
 func (g *Game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, screenHeight int) {
 	return g.Width, g.Height
+}
+
+// An Object is something that can be seen and positioned in the game
+type Object struct {
+	Image  *ebiten.Image
+	Op     *ebiten.DrawImageOptions
+	Center image.Point
+}
+
+// NewObjectFromImage makes a new game Object with fields calculated from an
+// already loaded image
+func NewObjectFromImage(img *ebiten.Image) *Object {
+	return &Object{
+		Image:  img,
+		Op:     &ebiten.DrawImageOptions{},
+		Center: image.Pt(0, 0),
+	}
+}
+
+// Load an image from embedded FS into an ebiten Image object
+func loadImage(name string) *ebiten.Image {
+	log.Printf("loading %s\n", name)
+
+	file, err := assets.Open(name)
+	if err != nil {
+		log.Fatalf("error opening file %s: %v\n", name, err)
+	}
+	defer file.Close()
+
+	raw, err := png.Decode(file)
+	if err != nil {
+		log.Fatalf("error decoding file %s as PNG: %v\n", name, err)
+	}
+
+	return ebiten.NewImageFromImage(raw)
 }
