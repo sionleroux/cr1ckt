3076f9b3f8190f5745e1ba01552fed9b99f4d0dd
Siôn le Roux <sinisterstuf@gmail.com>
2021-11-29 00:52:45 +0100

    Add test cases for OverlapsTiles
    These test variations combinations of overlapping tiles of earth, water
    and non-tile spaces to verify the current system works as expected.  It
    also includes a case for matching a top-left tile first before other
    tiles as both this solution and the next one are expected to start
    checking the top-left corner first.

--- collision_test.go
+++ collision_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image"
 	"testing"
 
 	"github.com/solarlune/ldtkgo"
@@ -13,5 +14,38 @@ func TestImpassible(t *testing.T) {
 	}
 	if !Impassible(&ldtkgo.Tile{ID: IdEarth}) {
 		t.Error("Earth should be impassible")
+	}
+}
+
+// rect16 returns a 16x16 rectangle at the given coordinates
+func rect16(x, y int) image.Rectangle {
+	p := image.Pt(x, y)
+	return image.Rectangle{p, p.Add(image.Pt(16, 16))}
+}
+
+func TestOverlapsTiles(t *testing.T) {
+	const gridSize int = 16
+	tiles := []*ldtkgo.Tile{
+		&ldtkgo.Tile{ID: 0, Position: []int{0, 0}},
+		&ldtkgo.Tile{ID: 114, Position: []int{32, 48}},
+		&ldtkgo.Tile{ID: 0, Position: []int{48, 48}},
+	}
+	cases := []struct {
+		hitbox  image.Rectangle
+		want    *ldtkgo.Tile
+		comment string
+	}{
+		{rect16(0, 0), tiles[0], "inside earth"},
+		{rect16(15, 15), tiles[0], "just inside earth"},
+		{rect16(16, 16), nil, "next to earth"},
+		{rect16(33, 33), tiles[1], "touching water"},
+		{rect16(40, 43), tiles[1], "touching water before earth"},
+		{rect16(50, 50), tiles[2], "touching other earth"},
+		{rect16(96, 96), nil, "far away"},
+	}
+	for _, c := range cases {
+		if res := OverlapsTiles(tiles, c.hitbox, gridSize); res != c.want {
+			t.Errorf("Collision %s is %v, want %v", c.comment, res, c.want)
+		}
 	}
 }
