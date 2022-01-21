bc250bce58d2f779555bf1377ce5a8d051055161
Siôn le Roux <sinisterstuf@gmail.com>
2022-01-22 00:32:52 +0100

    Add boilerplate project files for building Android distribution
    This is a basic Android project I got from Levi that imports the
    generated AAR file and runs it within a thin frame of Kotlin code.

---
+++ dist/android/app/src/main/java/org/sinisterstuf/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.sinisterstuf.myapplication
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import androidx.appcompat.app.AppCompatActivity
+import go.Seq
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Seq.setContext(applicationContext)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main)
+    }
+}
