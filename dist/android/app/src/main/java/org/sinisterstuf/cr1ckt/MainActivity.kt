cb0226643737fc000157942b17cc470a3f0bf4ee
Siôn le Roux <sinisterstuf@gmail.com>
2022-01-31 21:46:52 +0100

    Force hiding the action bar on phones that don't follow the layout
    I tried this on my older phone and even though the layout is specified
    as "no action bar" it doesn't seem to have any effect and it appears all
    the same.  Adding this extra line to turn it off again during onCreate
    solves it for that phone too.

--- dist/android/app/src/main/java/org/sinisterstuf/cr1ckt/MainActivity.kt
+++ dist/android/app/src/main/java/org/sinisterstuf/cr1ckt/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
+        getSupportActionBar()?.hide();
     }
 
     override fun onPause() {
